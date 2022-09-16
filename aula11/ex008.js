9536813bf5cec046d513db4606a9342ca774e0ef
Murilo Oliveira <murilonobrega8@gmail.com>
2022-09-16 20:48:25 -0300

    created_CondicionaisSimples

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+console.log('O console funcionou corretamente')
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}km/h!`)
+if (vel > 60) { // Condição simples
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+} else {
+    console.log('Velocidade dentro do limite da via! Dirija sempre usando cinto de segurança!')
+}
+
+
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') { // Pode usar o != (diferente) também!
+    console.log('Brasileiro!')
+} else {
+    console.log('Estrangeiro!')
+}
