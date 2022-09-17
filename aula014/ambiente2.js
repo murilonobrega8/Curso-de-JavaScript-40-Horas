0410590ce386352ca2469025e897f68b4af47612
Murilo Oliveira <murilonobrega8@gmail.com>
2022-09-17 16:15:40 -0300

    created_RepetiçãoFor

---
+++ aula014/ambiente2.js
@@ -0,0 +1,13 @@
+console.log('Vai começar...')
+for (var c = 1; c <= 6; c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('FIM!')
+
+//--------#--------
+
+/*var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
